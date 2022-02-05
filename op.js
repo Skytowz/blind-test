6618f42936928a6beb51637cfe8ebdc27df26d78
Skytowz <lucashottin359@gmail.com>
2022-02-05 18:36:41 +0100

    ajout MHA, FMAB

--- op.js
+++ op.js
@@ -1,22 +1,36 @@
 const data = {
-  Naruto: {
-    1: "https://www.youtube.com/watch?v=4t__wczfpRI",
-    2: "https://www.youtube.com/watch?v=SRn99oN1p_c",
+  "Naruto": {
+    "1": "https://www.youtube.com/watch?v=4t__wczfpRI",
+    "2": "https://www.youtube.com/watch?v=SRn99oN1p_c",
   },
   "One Piece": {
-    1: "https://www.youtube.com/watch?v=HRaoYuRKBaA",
-    2: "https://www.youtube.com/watch?v=54dp8ucsGG8",
+    "1": "https://www.youtube.com/watch?v=HRaoYuRKBaA",
+    "2": "https://www.youtube.com/watch?v=54dp8ucsGG8",
   },
   "My Hero Academia": {
-    1: "https://www.youtube.com/watch?v=yu0HjPzFYnY",
-    2: "https://www.youtube.com/watch?v=Q7w5IMyJ3pM",
+    "1": "https://www.youtube.com/watch?v=yu0HjPzFYnY",
+    "2": "https://www.youtube.com/watch?v=Q7w5IMyJ3pM",
+    "3": "https://www.youtube.com/watch?v=2ieT6Vh7jZE",
+    "4": "https://www.youtube.com/watch?v=iBRH9Po3gw8",
+    "5": "https://www.youtube.com/watch?v=WUDWBg4cFiw",
+    "6": "https://www.youtube.com/watch?v=VtNgeDxYJzE",
+    "7": "https://www.youtube.com/watch?v=MdfFVdFWl1U",
+    "8": "https://www.youtube.com/watch?v=jlwOlzVK5kY",
+    "9": "https://www.youtube.com/watch?v=KI17EkQBEc0"
   },
   "Kaguya-Sama": {
-    1: "https://www.youtube.com/watch?v=kkVNjjUgYdY",
-    2: "https://www.youtube.com/watch?v=lTlzDfhPtFA",
+    "1": "https://www.youtube.com/watch?v=W9Pb412Bloc",
+    "2": "https://www.youtube.com/watch?v=lTlzDfhPtFA",
   },
   "Naruto Shippuden": {
-    1: "https://www.youtube.com/watch?v=vxvP9zSOL7s",
-    2: "https://www.youtube.com/watch?v=r6EPMvIhtyA",
+    "1": "https://www.youtube.com/watch?v=vxvP9zSOL7s",
+    "2": "https://www.youtube.com/watch?v=r6EPMvIhtyA",
   },
+  "Full Metal Alchemist Brotherhood":{
+    "1": "https://www.youtube.com/watch?v=ShEAiFqkY0E",
+    "2": "https://www.youtube.com/watch?v=HusCOOG9e5Q",
+    "3": "https://www.youtube.com/watch?v=et7oxRRkPEo",
+    "4": "https://www.youtube.com/watch?v=KMZBauH47Jo",
+    "5": "https://www.youtube.com/watch?v=Es6cGvAv-pc",
+  }
 };
