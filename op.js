8561be2aca69f6e1776573f9e7efa5a10a0b69fe
Skytowz <lucashottin359@gmail.com>
2022-02-07 14:11:27 +0100

    Ajout SNK

--- op.js
+++ op.js
@@ -1,31 +1,4 @@
 const data = {
-  "Naruto": {
-    "1": "https://www.youtube.com/watch?v=4t__wczfpRI",
-    "2": "https://www.youtube.com/watch?v=SRn99oN1p_c",
-  },
-  "One Piece": {
-    "1": "https://www.youtube.com/watch?v=HRaoYuRKBaA",
-    "2": "https://www.youtube.com/watch?v=54dp8ucsGG8",
-  },
-  "My Hero Academia": {
-    "1": "https://www.youtube.com/watch?v=yu0HjPzFYnY",
-    "2": "https://www.youtube.com/watch?v=Q7w5IMyJ3pM",
-    "3": "https://www.youtube.com/watch?v=2ieT6Vh7jZE",
-    "4": "https://www.youtube.com/watch?v=iBRH9Po3gw8",
-    "5": "https://www.youtube.com/watch?v=WUDWBg4cFiw",
-    "6": "https://www.youtube.com/watch?v=VtNgeDxYJzE",
-    "7": "https://www.youtube.com/watch?v=MdfFVdFWl1U",
-    "8": "https://www.youtube.com/watch?v=jlwOlzVK5kY",
-    "9": "https://www.youtube.com/watch?v=KI17EkQBEc0"
-  },
-  "Kaguya-Sama": {
-    "1": "https://www.youtube.com/watch?v=W9Pb412Bloc",
-    "2": "https://www.youtube.com/watch?v=lTlzDfhPtFA",
-  },
-  "Naruto Shippuden": {
-    "1": "https://www.youtube.com/watch?v=vxvP9zSOL7s",
-    "2": "https://www.youtube.com/watch?v=r6EPMvIhtyA",
-  },
   "Full Metal Alchemist Brotherhood":{
     "1": "https://www.youtube.com/watch?v=ShEAiFqkY0E",
     "2": "https://www.youtube.com/watch?v=HusCOOG9e5Q",
@@ -45,4 +18,40 @@ const data = {
     "9": "https://www.youtube.com/watch?v=jcbKaK-oK2o",
     "10":"https://www.youtube.com/watch?v=1dPn7GvdfnI",
   },
+  "Kaguya-Sama": {
+    "1": "https://www.youtube.com/watch?v=W9Pb412Bloc",
+    "2": "https://www.youtube.com/watch?v=lTlzDfhPtFA",
+  },
+  "My Hero Academia": {
+    "1": "https://www.youtube.com/watch?v=yu0HjPzFYnY",
+    "2": "https://www.youtube.com/watch?v=Q7w5IMyJ3pM",
+    "3": "https://www.youtube.com/watch?v=2ieT6Vh7jZE",
+    "4": "https://www.youtube.com/watch?v=iBRH9Po3gw8",
+    "5": "https://www.youtube.com/watch?v=WUDWBg4cFiw",
+    "6": "https://www.youtube.com/watch?v=VtNgeDxYJzE",
+    "7": "https://www.youtube.com/watch?v=MdfFVdFWl1U",
+    "8": "https://www.youtube.com/watch?v=jlwOlzVK5kY",
+    "9": "https://www.youtube.com/watch?v=KI17EkQBEc0"
+  },
+  "Naruto": {
+    "1": "https://www.youtube.com/watch?v=4t__wczfpRI",
+    "2": "https://www.youtube.com/watch?v=SRn99oN1p_c",
+  },
+  "Naruto Shippuden": {
+    "1": "https://www.youtube.com/watch?v=vxvP9zSOL7s",
+    "2": "https://www.youtube.com/watch?v=r6EPMvIhtyA",
+  },
+  "One Piece": {
+    "1": "https://www.youtube.com/watch?v=HRaoYuRKBaA",
+    "2": "https://www.youtube.com/watch?v=54dp8ucsGG8",
+  },
+  "Shingeki no Kyojin": {
+    "1": "https://www.youtube.com/watch?v=8OkpRK2_gVs",
+    "2": "https://www.youtube.com/watch?v=PbWFpzi8C94",
+    "3": "https://www.youtube.com/watch?v=LKP-vZvjbh8",
+    "4": "https://www.youtube.com/watch?v=swwUoExDIng",
+    "5": "https://www.youtube.com/watch?v=0dK7JgKivQM",
+    "6": "https://www.youtube.com/watch?v=rmgSeLqsduM",
+    "7": "https://www.youtube.com/watch?v=9l9Wa-5ph6o",
+  }
 };
